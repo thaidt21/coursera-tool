92e1ad4cbc7101daaf8994ab55a2df0c2932cc50
thaidt <thaidt15@gmail.com>
2025-09-09 13:28:47 +0700

    first commit

---
+++ src/test/java/com/courseratoolapi/CourseraToolApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.courseratoolapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseraToolApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
